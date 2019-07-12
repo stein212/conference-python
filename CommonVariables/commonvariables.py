a31f0ef3291a77f66261d2465f8afcf4c354b2ce
APPLE <apple@APPLEs-Mac-mini-pk.local>
2019-07-12 11:05:17 +0530

    New Test case1

--- CommonVariables/commonvariables.py
+++ CommonVariables/commonvariables.py
@@ -1,6 +1,13 @@
-baseDirectory = "/opt/isls/isls/"
 #baseDirectory = "/users/apple/python/isls"
-ipAddress = "192.154.228.148"
-dbName = "dbtest"
-userName = "test1"
-password = "Test@123"
+# ipAddress = "192.154.228.148"
+# dbName = "dbtest"
+# userName = "test1"
+# password = "Test@123"
+
+
+
+baseDirectory = "/opt/isls/isls/"
+userName = "root"
+password = "password"
+ipAddress = "192.168.70.15"
+dbName = "dbtest1"
