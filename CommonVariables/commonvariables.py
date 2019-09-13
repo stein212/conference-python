7ee208755d56698276fce56ec1ef2db21a55ed89
praveen3edge <49227585+praveen3edge@users.noreply.github.com>
2019-09-13 15:09:16 +0530

    13/sep-poll

--- CommonVariables/commonvariables.py
+++ CommonVariables/commonvariables.py
@@ -1,14 +1,14 @@
-# baseDirectory = "/opt/isls/isls/"
-# ipAddress = "192.154.228.148"
-# dbName = "dbtest"
-# userName = "test1"
-# password = "Test@123"
-
-
-
-
-baseDirectory = "/users/apple/Azure/Test1/"
-userName = "root"
-password = "password"
-ipAddress = "192.168.43.125"
+baseDirectory = "/opt/isls/isls/"
+ipAddress = "192.154.228.148"
 dbName = "dbtest"
+userName = "test1"
+password = "Test@123"
+
+
+
+
+# baseDirectory = "/users/apple/Azure/Test1/"
+# userName = "root"
+# password = "password"
+# ipAddress = "192.168.43.125"
+# dbName = "dbtest"
