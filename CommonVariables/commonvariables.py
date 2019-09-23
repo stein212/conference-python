95a9cd76de86485c2980102be4b9a809ff4c9479
praveen3edge <49227585+praveen3edge@users.noreply.github.com>
2019-09-23 19:59:01 +0530

    23sep

--- CommonVariables/commonvariables.py
+++ CommonVariables/commonvariables.py
@@ -1,14 +1,14 @@
-# baseDirectory = "/opt/conference/conference/"
-# ipAddress = "155.254.17.159"
-# dbName = "dbtest1"
-# userName = "dbtest"
-# password = "dbtest"
+baseDirectory = "/opt/conference/conference/"
+ipAddress = "155.254.17.159"
+dbName = "dbtest1"
+userName = "dbtest"
+password = "dbtest"
 
 
 
 
-baseDirectory = "/users/apple/Azure/Test1/"
-userName = "root"
-password = "password"
-ipAddress = "192.168.70.123"#"192.168.70.123"#"192.168.43.125"
-dbName = "dbtest"
+# baseDirectory = "/users/apple/Azure/Test1/"
+# userName = "root"
+# password = "password"
+# ipAddress = "192.168.70.123"#"192.168.70.123"#"192.168.43.125"
+# dbName = "dbtest"
