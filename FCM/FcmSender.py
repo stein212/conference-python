fcb2018e28738ebc3cddd48cc2bfe4115dbc668e
praveen3edge <49227585+praveen3edge@users.noreply.github.com>
2019-10-04 19:02:19 +0530

    commit

--- FCM/FcmSender.py
+++ FCM/FcmSender.py
@@ -2,15 +2,19 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from pyfcm import FCMNotification
 
-push_service = FCMNotification(api_key="AAAAgS5PhGM:APA91bEwD_2_vVOhlNFifaQkLpHSFrD9faFuIUv5RN6WHKcTyoB3xFzqswnKha8vYTQu87mwEGhNGYaSqxxsbcgUgMMVw4j3Gns8JRX4EkSpzL9VSqEu974FzIoenl8KrIj1_qsjjZ_Q")
+push_service = FCMNotification(api_key="AAAAbyL8xP4:APA91bGR-5gW5_yHzD8CuQYgiko11sJ_tzo9Oupp1XjAZBaxHpYLXaUz9M4vjOA49spcm296a3N3q5C98qeG0GDRa29S44Py3XBfXSbP1K1o7sX9L_jOBe0PV2oElanPVOAlPKaK-JTW")
 
 #push_service = FCMNotification(api_key="<api-key>", proxy_dict={})
 
 class SendingNotification(Resource):
+    def __init__(self,**kwargs):
+        self.db = kwargs['data']
     def post(self):
-        registration_id = "fmEWhwrcVsE:APA91bE65_mHNcBMPjG8uZto_pWiAoexEOk3pFFbua837BStw_Y2mF_IFvESFTft4GRWDMhBpP801ToPreVGAMfM1kba2U-jI48_xtaw8bc_3Jm9XRpRH2LlB6FMwPt-lsBF8RRuTIvU"
-        message_title = "Uber update"
-        message_body = "Hi john, your customized news for today is ready"
+        userData = request.get_json(force=True)
+        registration_id = "dLtl5QjoVIE:APA91bHvXmfh3TZTTVlKAMaxZDjeJuy8ynraEA3-7p6UqWon_5xAc-DWHjwe_6SZmdB_AZXKvat-YGA3ukhjzLLnVLHjcsrlmlU10nMw5rZMQ1DOJAQQx4Nd64HZPdsHCIi6qSJB8_LF"
+        message_title = str(userData["title"])
+        message_body = str(userData["message"])
         result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
-        return {"msg":"Sent"}
+        # result = push_service.notify_multiple_devices()
+        return {"msg":result} 
 
